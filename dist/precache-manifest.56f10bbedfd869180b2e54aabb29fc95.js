3dc9aa5fe922a4ab6d56a5490e5e1b5d3cf60243
root <root@N-5CG0501P31.nsn-intra.net>
2021-06-11 01:08:12 +0530

    Build for heroku

---
+++ dist/precache-manifest.56f10bbedfd869180b2e54aabb29fc95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150bfbb10f5db28edf86",
+    "url": "/css/index.8db3a2e7.css"
+  },
+  {
+    "revision": "bb1beba1f5e651cce1f08bc080b212d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "272c0a726dbf1ca289cd",
+    "url": "/js/chunk-vendors.e33df7b7.js"
+  },
+  {
+    "revision": "150bfbb10f5db28edf86",
+    "url": "/js/index.6bd3770c.js"
+  },
+  {
+    "revision": "71833c14f842e294dc71967496b7e3a2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require ('express')
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
