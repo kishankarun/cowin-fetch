6e1379c5f1c72ce4f322080f17bd2bcd0cc05616
root <root@N-5CG0501P31.nsn-intra.net>
2021-06-11 01:08:56 +0530

    Build for heroku

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + "/dist/index.html");
 });
+
+app.listen(port);
+
+console.log("Server started");
