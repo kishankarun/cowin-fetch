38d553ae75a049b704a12a8fc3a191a0574db2fa
Kishan K <kishan.karun@gmail.com>
2021-06-18 13:07:50 +0530

    deploy

---
+++ precache-manifest.a135c8a67b2b214ebcbaf38eee724859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48888fba8074aab0456",
+    "url": "/css/index.5d501a87.css"
+  },
+  {
+    "revision": "7d98af7f9b03fe6e1ac7e6d0aad20599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7e7392f8b88abd9b453",
+    "url": "/js/chunk-vendors.c2f1a400.js"
+  },
+  {
+    "revision": "b48888fba8074aab0456",
+    "url": "/js/index.fdbd0c2f.js"
+  },
+  {
+    "revision": "1cfd9ae81d1f6effcd2bc9b2b6623cf7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
